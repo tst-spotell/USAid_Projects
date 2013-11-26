0bfbbe8b43a4c81fa943e5be83be0eaaf27bbdfe
Steve Potell <stevepotell@gmail.com>
2013-11-26 11:33:32 -0500

    Added the new fragments for displaying countries and projects with in countries

---
+++ USAidProjects/src/com/tscience/usaidprojects/USAidCountryListFragment.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2013 Thermopylae Sciences and Technology. All rights reserved.
+ */
+package com.tscience.usaidprojects;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * This is the fragment for displaying country search results.
+ * 
+ * @author spotell at t-sciences.com
+ */
+public class USAidCountryListFragment extends Fragment {
+
+} // end USAidCountryListFragment
