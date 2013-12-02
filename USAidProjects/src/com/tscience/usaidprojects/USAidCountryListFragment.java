86694482bd6d247554b38a78cf02b5a14c813381
Steve Potell <spotell@t-sciences.com>
2013-12-02 10:06:14 -0500

    Setting up country list

--- USAidProjects/src/com/tscience/usaidprojects/USAidCountryListFragment.java
+++ USAidProjects/src/com/tscience/usaidprojects/USAidCountryListFragment.java
@@ -3,13 +3,33 @@
  */
 package com.tscience.usaidprojects;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockListFragment;
 
 /**
  * This is the fragment for displaying country search results.
  * 
  * @author spotell at t-sciences.com
  */
-public class USAidCountryListFragment extends Fragment {
+public class USAidCountryListFragment extends SherlockListFragment {
+    
+    /** Log id of this class name. */
+    private static final String LOG_TAG = "USAidCountryListFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        
+        // layout the view
+        View listView = inflater.inflate(R.layout.usaid_country_layout, container, false);
+        
+        // TODO load data from country list
+        
+        return listView;
+        
+    } // end onCreateView
 
 } // end USAidCountryListFragment
