131dfd96aa1e3a0f77de4aca0041a46479171b6f
Steve Potell <spotell@t-sciences.com>
2014-01-06 10:39:27 -0500

    more sector list

--- USAidProjects/src/com/tscience/usaidprojects/USAidSectorListAdapter.java
+++ USAidProjects/src/com/tscience/usaidprojects/USAidSectorListAdapter.java
@@ -3,6 +3,8 @@
  */
 package com.tscience.usaidprojects;
 
+import java.util.ArrayList;
+
 import com.tscience.usaidprojects.utils.USAidProjectsSnapshotObject;
 
 import android.content.Context;
@@ -10,6 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -20,6 +25,8 @@ import android.widget.ArrayAdapter;
  */
 public class USAidSectorListAdapter extends ArrayAdapter<USAidProjectsSnapshotObject> {
 
+    private ArrayList<USAidProjectsSnapshotObject> items;
+    
     // the view inflater used by this adapter
     private LayoutInflater inflater;
     
@@ -29,8 +36,10 @@ public class USAidSectorListAdapter extends ArrayAdapter<USAidProjectsSnapshotOb
      * @param context
      * @param resource
      */
-    public USAidSectorListAdapter(Context context, int resource) {
-        super(context, resource);
+    public USAidSectorListAdapter(Context context, int resource, ArrayList<USAidProjectsSnapshotObject> value) {
+        super(context, resource, value);
+        
+        items = value;
         
         // create the inflater
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -40,11 +49,51 @@ public class USAidSectorListAdapter extends ArrayAdapter<USAidProjectsSnapshotOb
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         
+        View currentView = convertView;
         
+        // if the view has not been created create it
+        if (currentView == null) {
+            
+            currentView = inflater.inflate(R.layout.usaid_sector_item, null);
+            
+            // create the tag we are using
+            currentView.setTag(new USAidSectorHolder());
+            
+        }
         
+        USAidSectorHolder usaidSectorHolder = (USAidSectorHolder) currentView.getTag();
         
-        return super.getView(position, convertView, parent);
+        // load the holder if empty
+        if (usaidSectorHolder.sectorNameView == null) {
+            
+//            usaidSectorHolder.typeImageView = (ImageView) currentView.findViewById(R.id.usaid_country_flag);
+            usaidSectorHolder.sectorNameView = (TextView) currentView.findViewById(R.id.sectorNameItem);
+            usaidSectorHolder.sectorCheckBox = (CheckBox) currentView.findViewById(R.id.sectorCheckbox);
+            
+        }
+        
+        // the usaid DataObject object we are working with
+        usaidSectorHolder.usaidDataObject = items.get(position);
+        
+        // TODO set the image
+        
+        // set the name
+        usaidSectorHolder.sectorNameView.setText(usaidSectorHolder.usaidDataObject.label);
+        
+        usaidSectorHolder.sectorCheckBox.setSelected(usaidSectorHolder.usaidDataObject.selected);
+        
+        return currentView;
         
     } // end getView
+    
+    static class USAidSectorHolder {
+        
+        ImageView typeImageView;
+        TextView sectorNameView;
+        CheckBox sectorCheckBox;
+        
+        USAidProjectsSnapshotObject usaidDataObject;
+        
+    }
 
 } // end USAidSectorListAdapter
